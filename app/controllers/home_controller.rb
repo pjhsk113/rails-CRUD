b17ea8e162b8bba45ac2fdd9aa3212ae0d364eab
Janghopark <pjhsk113@naver.com>
2019-10-22 12:58:47 +0000

    Update CRUD

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,39 @@ class HomeController < ApplicationController
 	def index
 		
 	end
+	def boardList
+		@board = Board.all
+	end
+	
+	def boardWrite
+		
+	end
+	
+	def WriteOk
+		board = Board.new
+		board.title = params[:title]
+		board.content = params[:content]
+		board.save
+		
+		redirect_to '/boardList'
+	end
+	
+	def boardEdit
+		@board = Board.find(params[:board_id])
+	end
+	
+	def EditOk
+		board = Board.find(params[:board_id])
+		board.title = params[:title]
+		board.content = params[:content]
+		board.save
+		
+		redirect_to '/boardList'
+	end
+	
+	def boardDelete
+		Board.destroy(params[:board_id])
+		
+		redirect_to '/boardList'
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	get '/home', to: 'home#index'
+	get '/boardList', to: 'home#boardList'
+	get '/boardWrite', to: 'home#boardWrite'
+	get '/WriteOk', to: 'home#WriteOk'
+	get '/boardEdit/:board_id', to: 'home#boardEdit'
+	get '/EditOk/:board_id', to: 'home#EditOk'
+	get '/boardDelete/:board_id', to: 'home#boardDelete'
 end
